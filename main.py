7f35d856e1e3561cd1ad99a276c28ede53adf585
Athallah Nuzul <athallahnuzul31104@gmail.com>
2022-11-11 09:37:13 +0700

    First Project

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Ini adalah project pertama dengan python
 '''
 print("Hello world!")
 print("My name is Athallah Nuzul")
-print(" Im gonna learny python for remote worker")
+print(" Im gonna learn python for remote worker")
 
 
 
