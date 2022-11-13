ba349beb94d3f75f5deeadb83fd83fb9065595cc
Athallah Nuzul <athallahnuzul31104@gmail.com>
2022-11-14 04:13:33 +0700

    First Project

--- Main.py
+++ Main.py
@@ -4,3 +4,9 @@ Semua sintaksis dasar terdiri dari:
 2. Percabangan : Langkah melompat jika kondisi terpenuhi
 3. Perulangan : Mengulang langkah yang sama berkali kali sampai konddisi terpenuhi
 """
+# Sequensial
+print('Ibu berkata,"Pergi ke toko"')
+print('Budi menjawab,"Baik, apa yang harus dilakukan di toko?"')
+print('Ibu menjawab,"Beli satu botol susu, dan jika ada telor beli enam"')
+print("Maka Budi berangkat ke toko ")
+print("Dan mulai berbelanja")
