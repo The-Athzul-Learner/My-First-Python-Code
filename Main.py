6de6ef99549266647c08a9bb47c747973a17fe58
Athallah Nuzul <athallahnuzul31104@gmail.com>
2022-11-14 03:35:26 +0700

    First Project

---
+++ Main.py
@@ -0,0 +1,6 @@
+"""
+Semua sintaksis dasar terdiri dari:
+1. Sequensial : Langkah mengulang
+2. Percabangan : Langkah melompat jika kondisi terpenuhi
+3. Perulangan : Mengulang langkah yang sama berkali kali sampai konddisi terpenuhi
+"""
--- main.py
+++
@@ -1,9 +0,0 @@
-'''
-Ini adalah project pertama dengan python
-'''
-print("Hello world!")
-print("My name is Athallah Nuzul")
-print(" Im gonna learn python for remote worker")
-
-
-
